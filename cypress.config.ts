af1f6c807b06b484dbe2d47f4430c28b1f2e2997
jkenjayev <javohirwebdev@gmail.com>
2023-09-13 13:24:04 +0500

    modify: added result folders in .gitignore file

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,5 +8,12 @@ export default defineConfig({
     overwrite: false,
     html: false,
     json: true,
+    charts: true,
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
