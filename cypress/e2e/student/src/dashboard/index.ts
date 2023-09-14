3ffc596a516a75d9655e35978629fd97c6715001
jkenjayev <javohirwebdev@gmail.com>
2023-09-14 22:48:41 +0500

    modification

--- cypress/e2e/student/src/dashboard/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./recBlog.action";
-export * from "./recCourses.action";
-export * from "./lastSeen.action";
-export * from "./sidebarCourses.action";
--- cypress/support/repositories/student/dashboard/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./recBlog.repo";
-export * from "./recCourses.repo";
-export * from "./lastSeen.repo";
-export * from "./sidebarCourses.repo";
