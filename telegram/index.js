299e448590b540c3f536c539aee0acbd67e471b4
jkenjayev <javohirwebdev@gmail.com>
2023-11-19 16:29:20 +0500

    fix: form-data package installed

--- telegram/index.js
+++ telegram/index.js
@@ -1,35 +1,34 @@
-/// <reference types="cypress" />
 const axios = require("axios");
+const path = require("path");
 const FormData = require("form-data");
-const {resolve} = require("node:path");
+const { resolve } = require("node:path");
 const moment = require("moment-timezone");
-const fs = require("node:fs")
-const fileToSend = resolve("cypress/reports.zip");
-const caption = 'test run date: ' + createExecuteTime()
+const fs = require("node:fs");
+const fileToSend = path.join(path.resolve(__dirname, "../cypress/reports.zip"));
+const caption = "test run date: " + createExecuteTime();
 
 const token = "6681396777:AAF6iYwVjMvyq9lyjNBhDZEJwBKHO9odZNo";
-const chatId = '-1001913146155';
-
+const chatId = "-1001913146155";
 const formData = new FormData();
-formData.append("document", fs.createReadStream(fileToSend))
-formData.append('chat_id', chatId);
-formData.append("caption", caption)
+formData.append("document", fs.createReadStream(fileToSend));
+formData.append("chat_id", chatId);
+formData.append("caption", caption);
 
 axios.post(`https://api.telegram.org/bot${token}/sendDocument`, formData, {
-		headers: {
-			...formData.getHeaders(),
-		},
-	})
-	.then((response) => {
-		console.log(response.data);
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+    headers: {
+      // ...formData.getHeaders()
+    }
+  })
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 
 function createExecuteTime() {
-	const tashkentTime = moment.tz('Asia/Tashkent');
-	return tashkentTime.format("DD-MMM-YYYY HH:mm")
+  const tashkentTime = moment.tz("Asia/Tashkent");
+  return tashkentTime.format("DD-MMM-YYYY HH:mm");
 }
 
